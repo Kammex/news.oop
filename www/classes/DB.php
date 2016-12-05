f3312f9c3f641ee498cb7f25618e7ee21518d7f5
oleg <antykuz@i.ua>
2016-12-05 21:52:23 +0200

    Вывод всех новостей из базы на главную страницу

---
+++ www/classes/DB.php
@@ -0,0 +1,58 @@
+<?php
+
+class DB
+{
+    private $link;
+
+    public function __construct()
+    {
+        $this->link = mysqli_connect('localhost', 'root', '1234', 'test');
+        mysqli_query($this->link, 'SET NAMES utf8');
+        return $this->link;
+    }
+
+    public function query($sql, $class = 'stdClass')
+    {
+        $res = mysqli_query($this->link, $sql);
+        if (false === $res) {
+            return false;
+        }
+
+        $ret = [];
+        while ($row = mysqli_fetch_object($res, $class)) {
+            $ret[] = $row;
+        }
+        return $ret;
+    }
+}
+
+/*
+class DB
+{
+    private $link;
+
+    public function __construct()
+    {
+        return $this->link = mysqli_connect('localhost', 'root', '1234', 'test');
+    }
+
+    public function queryAll($sql, $class = 'stdClass')
+    {
+        $res = mysqli_query($this->link, $sql);
+        if (false === $res) {
+            return false;
+        }
+
+        $ret = [];
+        while ($row = mysqli_fetch_object($res, $class)) {
+            $ret[] = $row;
+        }
+        return $ret;
+    }
+
+    public function queryOne($sql, $class = 'stdClass')
+    {
+        return $this->queryAll($sql, $class)[0];
+    }
+}
+*/
--- www/index.php
+++ www/index.php
@@ -1,6 +1,35 @@
 <?php
 
-echo 'Hello, world';
-echo 'One';
+require_once __DIR__ . '/models/News.php';
 
-?>
+$items = News::getAll();
+
+include __DIR__ . '/views/index.php';
+
+
+/*Front-controller
+
+require_once __DIR__ . '/autoload.php';
+
+/*С помощью метода Get передаются имя контроллера и выполняемого метода
+/*Создаются переменные, содержащие переданые класс и метод, при этом назначаются значения по умолчанию,
+такие как 'News' & 'All'
+$ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
+$act = isset($_GET['act']) ? $_GET['act'] : 'All';
+
+/*Формируется имя класса контролера
+$controllerClassName = $ctrl . 'Controller';
+
+/*Автоматическое подключение необходимого класса файла
+//require_once __DIR__ . '/controllers/' . $controllerClassName . '.php';
+
+/*Создание переменной заданного класса
+$controller = new $controllerClassName;
+
+/*Формирование переменной с необходимым методом
+$method = 'action' . $act;
+
+/*Вызов нужного метода
+$controller->$method();
+
+*/
